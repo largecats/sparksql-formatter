8b7fcb1827da27a66351d472fc46a857130704eb
largecats <linfanxiaolinda@outlook.com>
2020-07-08 00:56:31 +0800

    fix bug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='hiveqlformatter-largecats',  # Replace with your own username
-    version='0.0.14',
+    version='0.0.15',
     author='largecats',
     author_email='linfanxiaolinda@outlook.com',
     description=
