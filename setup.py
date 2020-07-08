c7c9cca6becacc0e2d68e722dee8bb856d4926ec
largecats <linfanxiaolinda@outlook.com>
2020-07-08 23:24:33 +0800

    bumped patch number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='hiveqlformatter-largecats',  # Replace with your own username
-    version='0.0.20',
+    version='0.0.21',
     author='largecats',
     author_email='linfanxiaolinda@outlook.com',
     description=
