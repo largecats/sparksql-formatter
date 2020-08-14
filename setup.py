ba35f685729e5c0025013a0704f51484425b241e
xiaolf <xiaolf@seagroup.com>
2020-08-14 12:43:42 +0800

    Edited version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='sparksqlformatter',
-    version='0.0.2',
+    version='0.1.1',
     author='largecats',
     author_email='linfanxiaolinda@outlook.com',
     description=
