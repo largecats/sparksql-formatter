93f5ad5398e9c0ad642e5e1b3b49c57c3d7059a5
xiaolf <xiaolf@seagroup.com>
2020-08-27 12:42:35 +0800

    Limit opening/closing parentheses to (, ) in subquery formatting

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='sparksqlformatter',
-    version='0.1.6',
+    version='0.1.7',
     author='largecats',
     author_email='linfanxiaolinda@outlook.com',
     description=
